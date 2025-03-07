20d8dc5150f356f31ec5a447e5a67f05f70e68e8
JushrafRahman <jushrafrahi97@gmail.com>
2025-03-06 23:28:49 -0500

    add new prompts to db

--- index.ts
+++ index.ts
@@ -2,15 +2,17 @@ import 'dotenv/config'
 import { runLLM } from './src/llm'
 import {addMessages, getMessages, removeMetaData} from './src/memory';
 
-const userMessage = process.argv[2]
+const userMessage = process.argv[2] // given prompt
 
 if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
 
+await addMessages([{ role: 'user', content: userMessage }]); // add the new message given in terminal to the db
+
 const history = await getMessages();
 
-const response = await runLLM({ messages: [...history, { role: 'user', content: userMessage }]});
+const response = await runLLM({ messages: history }); // send all messages from beginning as context
 
 console.log(response)
