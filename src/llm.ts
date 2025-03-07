5397bfd489ecf11ab1c07cea2814b3f85776139e
JushrafRahman <jushrafrahi97@gmail.com>
2025-03-06 22:43:53 -0500

    fixup

--- src/llm.ts
+++ src/llm.ts
@@ -1,7 +1,7 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
 
-export const runLLM = async ( messages : AIMessage[]) => {
+export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
